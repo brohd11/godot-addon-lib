f0ac98d1814329c309066eca93d475fc8162530c
brohd <brohdielr@proton.me>
2026-09-18 13:11:43 -0700

    remove dock manager

--- alib_runtime/utils/u_version.gd
+++ alib_runtime/utils/u_version.gd
@@ -1,21 +1,2 @@
 #! namespace ALibRuntime.Utils class UVersion
-
-static var _version_info:= {}
-
-static func get_major_version():
-	if not _version_info.is_empty():
-		return _version_info.major
-	_version_info = Engine.get_version_info()
-	return _version_info.major
-
-static func get_minor_version():
-	if not _version_info.is_empty():
-		return _version_info.minor
-	_version_info = Engine.get_version_info()
-	return _version_info.minor
-
-static func get_patch():
-	if not _version_info.is_empty():
-		return _version_info.patch
-	_version_info = Engine.get_version_info()
-	return _version_info.patch
+extends UtilR.URVersion
